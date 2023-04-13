86c2aa3b7cb793cc05999425b0481ba64da51532
wzkMaster <727498469@qq.com>
2023-04-13 12:00:34 +0800

    解决问题

--- src/pages/api/genPic.ts
+++ src/pages/api/genPic.ts
@@ -1,22 +1,27 @@
 import type { APIRoute } from "astro";
-import { Configuration, OpenAIApi } from 'openai';
 
-const configuration = new Configuration({
-  apiKey: import.meta.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+const apiKey = import.meta.env.OPENAI_API_KEY;
 
 export const post: APIRoute = async (context) => {
   const body = await context.request.json();
   const { prompt, size } = body;
   try {
-    const response = await openai.createImage({
-      prompt,
-      n: 1,
-      size,
-    });
-
-    const imageUrl = response.data.data[0].url;
+    const response = await fetch(
+      "https://api.openai.com/v1/images/generations",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${apiKey}`,
+        },
+        method: "POST",
+        body: JSON.stringify({
+          prompt,
+          n: 1,
+          size
+        }),
+      }
+    );
+    const imageUrl = (await response.json()).data[0].url;
 
     return new Response(
       JSON.stringify({
